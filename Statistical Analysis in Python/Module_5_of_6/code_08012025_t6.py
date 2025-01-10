919cbac37d7172d09165f8487b8d7a73bb682f3d
rafal_szponarski <rafal.szponarski@o2.pl>
2025-01-10 20:44:24 +0100

    Task6 M5 explanation added

--- Statistical Analysis in Python/Module_5_of_6/code_08012025_t6.py
+++ Statistical Analysis in Python/Module_5_of_6/code_08012025_t6.py
@@ -10,9 +10,15 @@ X = pd.read_csv('pizza.csv').drop(['id', 'brand'], axis=1)
 # Performing the t-SNE transformation
 tsne = TSNE(n_components=2, random_state=42)
 X_tsne = tsne.fit_transform(X)
+'''TSNE(n_components=2): Konfiguracja algorytmu t-SNE -> redukuje wymiary danych do 2D, co umożliwia ich wizualizację
+na płaszczyźnie.
+random_state=42: Zapewnienie powtarzalności wyników przez ustalenie ziarna generatora liczb losowych.
+fit_transform(X): Algorytm uczy się struktury danych X i generuje nowe współrzędne w przestrzeni 2D (X_tsne).
+'''
 
 # Selecting a feature to colour - for example, "ash"
 feature_to_color = X['ash']
+'''Kolumna ash (np. zawartość popiołu w pizzy) wybrana jako cecha, według której będą kolorowane punkty na wykresie.'''
 
 # Drawing a t-SNE diagram with colouring according to the selected characteristic
 plt.figure(figsize=(10, 8))
@@ -21,4 +27,10 @@ plt.colorbar(scatter)
 plt.title('t-SNE visualisation with colouring by "ash" feature')
 plt.xlabel('t-SNE 1')
 plt.ylabel('t-SNE 2')
-plt.savefig('fig6.png')
+#plt.savefig('fig6.png')
+plt.show()
+'''NOTES:
+-SNE grupuje punkty, które są do siebie podobne w przestrzeni wielowymiarowej, w taki sposób, aby znajdowały się blisko
+siebie na wykresie 2D. Dzięki temu można wizualizować struktury i grupy w danych, które byłyby trudne do dostrzeżenia
+w przestrzeni o wysokiej liczbie wymiarów.
+'''
