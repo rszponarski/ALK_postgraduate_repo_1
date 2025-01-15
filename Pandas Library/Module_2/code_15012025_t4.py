713f118bd52894486e1a26e6d6a8b0f460cba6a6
rafal_szponarski <rafal.szponarski@o2.pl>
2025-01-15 14:44:37 +0100

    t3_m2 added

---
+++ Pandas Library/Module_2/code_15012025_t4.py
@@ -0,0 +1,12 @@
+# Module 2 Task 1
+"""
+Baza została podzielona na wiele plików JSON nazwanych beer_1.json, beer_2.json, itd.
+Rozejrzyj się w strukturze przykładowego pliku beer_1.json i
+    napisz funkcję: load_beer(dirpath), która wczyta wszystkie pliki z fragmentami tej bazy danych z podanego folderu
+    (argument dirpath to ścieżka do odpowiedniego folderu) i zwróci DataFrame zawierający wszystkie dane z pominięciem
+    metadanych samego pliku.
+-- Podpowiedź
+Pliki JSON często są zagnieżdżone i zawierają więcej niż jeden obiekt. Upewnij się, że został wybrany właściwy.
+-- Podpowiedź
+Przypomnij sobie różnice w działaniu metod .append() i .extend().
+"""
