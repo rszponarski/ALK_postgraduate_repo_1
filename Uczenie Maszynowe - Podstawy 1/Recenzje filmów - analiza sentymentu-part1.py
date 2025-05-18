ecd6076761fbb389604557c81e7f374b461d0cdf
rafal_szponarski <rafal.szponarski@o2.pl>
2025-05-18 11:06:04 +0200

    Eksploracyjna analiza danych tekstu 180525

--- Uczenie Maszynowe - Podstawy 1/Recenzje filmów - analiza sentymentu-part1.py
+++ Uczenie Maszynowe - Podstawy 1/Recenzje filmów - analiza sentymentu-part1.py
@@ -10,6 +10,7 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from collections import Counter
+import matplotlib.pyplot as plt
 
 imdb = pd.read_csv('imdb.csv')
 
@@ -58,3 +59,24 @@ word_freq_neg = Counter(all_words_neg)
 
 positive_common_words = word_freq_pos.most_common(20)
 negative_common_words = word_freq_neg.most_common(20)
+
+# Wizualizacja
+# dwa wykresy typu bar plot ze zliczeniem najczęściej występujących słów dla negatywnych i pozytywnych recenzji
+
+def plot_word_frequency(word_freq, title, ax):
+    word, count = zip(*word_freq)
+    ax.barh(word, count, color='green')
+    ax.set_xlabel('Częstość występowania słów')
+    ax.set_title(title)
+    ax.invert_yaxis()
+
+fig, axes = plt.subplots(1, 2, figsize=(15,6))
+
+plot_word_frequency(positive_common_words, "20 najczęściej występujących słów - recenzje pozytywne", ax=axes[0])
+plot_word_frequency(negative_common_words, "20 najczęściej występujących słów - recenzje negatywne", ax=axes[1])
+
+plt.savefig('most_common_words.png')
+
+# Stwórz kolumnę imdb['review_length'], która reprezentuje liczbę słów w każdej recenzji
+
+imdb['review_length'] = imdb['review_processed'].str.count(r'\w+')
